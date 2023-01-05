a49d4cd2831a125b25e78c3580ddeca5db91a5aa
Vovchik <vova2000ra@mail.ru>
2023-01-06 01:18:39 +0600

    about

--- js/script.js
+++ js/script.js
@@ -20,3 +20,29 @@ var swiper = new Swiper(".offer__wrapper", {
         }
     }
 });
+
+var swiper = new Swiper(".news__wrapper", {
+    slidesPerView: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".slider-next",
+        prevEl: ".slider-prev",
+    },
+    pagination: {
+        el: '.slider-pagination',
+        type: 'bullets',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
